f3ec8917f95e1a1ab9ad326a67059132e9f5edae
Vasanth11111 <106176312+Vasanth11111@users.noreply.github.com>
2022-05-25 09:13:28 +0530

    Update yourData.js

--- yourData.js
+++ yourData.js
@@ -114,8 +114,6 @@ export default {
 
   // About Secton --------------
   aboutpara:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "i am vasanth"
   //   End About Section ---------------------
 };
-
-// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
